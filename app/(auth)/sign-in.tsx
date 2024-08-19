e8e93f467540dd667b95cd75add128116d5e7ed4
Patricio Dieck <123036273+PatricioDieck@users.noreply.github.com>
2024-08-19 13:15:30 -0600

    setting up the authentication

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,16 +6,14 @@ import CustomButton from "@/components/CustomButton";
 import { Link } from "expo-router";
 import OAuth from "@/components/OAuth";
 
-const SignUp = () => {
+const SignIn = () => {
   const [form, setForm] = useState({
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
   });
 
-  const onSingUpPress = async () => {
-    console.log("Sign Up  Pressed");
+  const onSingInPress = async () => {
+    console.log("Sign In  Pressed");
   };
 
   return (
@@ -24,17 +22,10 @@ const SignUp = () => {
         <View className="relative w-full h-[250px] ">
           <Image source={images.signUpCar} className="w-full h-[250px] z-0" />
           <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-            Create Your Account
+            Welcome 👋
           </Text>
         </View>
         <View className="p-5 ">
-          <InputField
-            label="Name"
-            placeholder="Enter your name"
-            icon={icons.person}
-            value={form.name}
-            onChangeText={(value: string) => setForm({ ...form, name: value })}
-          />
           <InputField
             label="Email"
             placeholder="Enter your email"
@@ -52,18 +43,18 @@ const SignUp = () => {
             }
           />
           <CustomButton
-            title="Sign Up"
-            onPress={() => onSingUpPress()}
+            title="Sign In"
+            onPress={() => onSingInPress()}
             className="mt-6"
           />
           {/* OAuth */}
           <OAuth />
           <Link
-            href="/sign-in"
-            className="text-lg text-center text-general-200 mt-10"
+            href="/(auth)/sign-up"
+            className="text-lg text-center text-general-200 mt-10 "
           >
-            <Text className="">Already have an account?</Text>
-            <Text className="text-primary-500"> Sign In</Text>
+            <Text className="">Don't have an account?</Text>
+            <Text className="text-primary-500"> Sign Up</Text>
           </Link>
         </View>
         {/* verification modal */}
@@ -72,4 +63,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default SignIn;
